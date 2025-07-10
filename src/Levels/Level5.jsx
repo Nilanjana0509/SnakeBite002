14be0310704e4047f5b05c13851b82d93d9a62e2
Nilanjana Chaudhuri <145705332+Nilanjana0509@users.noreply.github.com>
2025-07-10 19:55:13 +0530

    Update Level5.jsx

--- src/Levels/Level5.jsx
+++ src/Levels/Level5.jsx
@@ -1,5 +1,3 @@
-// No sign of envenomation: NP
-
 import React, { useState, useEffect } from "react";
 import CustomAlert from "./CustomAlert"; // Importing the CustomAlert component
 import { useLocation, useNavigate } from "react-router-dom";
