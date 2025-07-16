f0368b77651af0903ca0f4daf0b407785abcdc80
Nilanjana Chaudhuri <145705332+Nilanjana0509@users.noreply.github.com>
2025-07-16 12:58:12 +0530

    Update Level11.jsx

--- src/Levels/Level11.jsx
+++ src/Levels/Level11.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import CustomAlert from "./CustomAlert"; // Importing the CustomAlert component
+import CustomAlert from "./CustomAlert";
 import { useLocation, useNavigate } from "react-router-dom";
 import { FaClock, FaQuestionCircle, FaStar } from "react-icons/fa";
 import backgroundImage from "../assets/images/snake11.png";
@@ -7,19 +7,16 @@ import backgroundImage from "../assets/images/snake11.png";
 const Level11 = ({ setCompletedLevels }) => {
   const navigate = useNavigate();
   const [deck, setDeck] = useState([]);
-  const [deckIndex, setDeckIndex] = useState(null); // Track the current deck index
+  const [deckIndex, setDeckIndex] = useState(null);
   const [selectedCards1, setSelectedCards1] = useState({});
   const [selectedCards2, setSelectedCards2] = useState({});
   const [selectedCards3, setSelectedCards3] = useState({});
   const [showSuccessPopup, setShowSuccessPopup] = useState(false);
   const [showWrongPopup, setShowWrongPopup] = useState(false);
   const [result, SetResult] = useState([]);
-  // const [countdown, setCountdown] = useState(1000);
-  const [level3Selection, setLevel3Selection] = useState(null);
   const [starCount, setStarCount] = useState(0);
 
   const handleCompleteLevel11 = () => {
-    // Mark level 10 as completed
     const completedLevels = {
       level1: true,
       level2: true,
@@ -41,26 +38,16 @@ const Level11 = ({ setCompletedLevels }) => {
     array.push(selectedCards3.text);
 
     console.log(array);
-    localStorage.setItem("origin", "level11", "level11Result", JSON.stringify(array));
+    localStorage.setItem("level11Result", JSON.stringify(array));
     setCompletedLevels(completedLevels);
 
-    // Navigate to level 11
-    // if (codeSelection()) {
-    //   // Navigate to Level 7
-    //   navigate("/level12");
-    // } else {
-    //   // Navigate to Level 8
-    //   navigate("/level15");
-    // }
+    navigate("/level12");
   };
   useEffect(() => {
-    // Save the current level path to localStorage
     localStorage.setItem('currentLevel', location.pathname);
-
-    // Retrieve current level from localStorage on reload
     const savedLevel = localStorage.getItem('currentLevel');
     if (savedLevel && savedLevel !== location.pathname) {
-      navigate(savedLevel); // Navigate to the saved level if it's different
+      navigate(savedLevel);
     }
     const data = JSON.parse(localStorage.getItem("path")) || {};
     const trueCount = Object.values(data).filter(value => value === true).length;
@@ -77,24 +64,12 @@ const Level11 = ({ setCompletedLevels }) => {
     { id: 7, text: "Till next 24 hours" },
   ];
 
-  // Correct sequence of cards
   const correctSequence = [
     { id: 1, text: "20 WBCT" },
     { id: 2, text: "Every hour" },
     { id: 3, text: "Till next 4 hours" },
   ];
 
-  // Shuffle the deck when the component mounts
-  // useEffect(() => {
-  //   const shuffledDeck = shuffle(Array.from(initialDeck.entries()));
-  //   setDeck(shuffledDeck);
-  // }, []);
-
-  // useEffect(() => {
-  //   setDeck(initialDeck); // Set the first card as the initial card
-  // }, []);
-
-  // Function to shuffle an array
   const shuffle = (array) => {
     const shuffledArray = [...array];
     for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -104,7 +79,6 @@ const Level11 = ({ setCompletedLevels }) => {
     return shuffledArray;
   };
 
-  // Set the shuffled deck when the component mounts
   useEffect(() => {
     const shuffledDeck = shuffle(initialDeck);
     setDeck(shuffledDeck);
@@ -120,112 +94,8 @@ const Level11 = ({ setCompletedLevels }) => {
     }
   }, [selectedCards1, selectedCards2, selectedCards3]);
 
-  // useEffect(() => {
-  //   if (countdown <= 0) {
-  //     resetGame(); // Reload the page when countdown reaches zero
-  //     return;
-  //   }
-
-  //   // Set the interval to decrease countdown every second (1000 ms)
-  //   const timer = setInterval(() => {
-  //     setCountdown((prev) => prev - 1);
-  //   }, 1000);
-
-  //   // Cleanup the interval on component unmount
-  //   return () => clearInterval(timer);
-  // }, [countdown]);
-
-  // Shuffle function
-  // const shuffle = (array) => {
-  //   for (let i = array.length - 1; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     [array[i], array[j]] = [array[j], array[i]];
-  //   }
-  //   return array;
-  // };
-
-  // function getRandomObject() {
-  //   const randomIndex = Math.floor(Math.random() * initialDeck.length);
-  //   return initialDeck[randomIndex];
-  // }
-
-  // const initialfun = () => {
-  //   setDeck(getRandomObject());
-  // };
-
-  // const getText1 = () => {
-  //   if (deck.text === undefined) {
-  //     alert("Please select the card from the deck");
-  //   } else {
-  //     setSelectedCards1(deck);
-  //     SetResult((prevResult) => [...prevResult, deck]);
-  //     initialfun();
-  //     // handleBoxClick(deck, setSelectedCards2, setSelectedCards3);
-  //   }
-  // };
-
-  // const getText2 = () => {
-  //   if (deck.text === undefined) {
-  //     alert("Please select the card from the deck");
-  //   } else {
-  //     setSelectedCards2(deck);
-  //     SetResult((prevResult) => [...prevResult, deck]);
-  //     initialfun();
-  //     // handleBoxClick(setSelectedCards1, deck, setSelectedCards3);
-  //   }
-  // };
-  // const getText3 = () => {
-  //   if (deck.text === undefined) {
-  //     alert("Please select the card from the deck");
-  //   } else {
-  //     setSelectedCards3(deck);
-  //     SetResult((prevResult) => [...prevResult, deck]);
-  //     initialfun();
-  //     // handleBoxClick(setSelectedCards1, setSelectedCards2, deck);
-  //   }
-  // };
-
-  // const res = () => {
-  //   // console.log('sdsds');
-  //   console.log(selectedCards1);
-  //   console.log(selectedCards2);
-  //   console.log(selectedCards3);
-
-  //   if (
-  //     selectedCards1.id === correctSequence[0].id &&
-  //     selectedCards2.id === correctSequence[1].id &&
-  //     selectedCards3.id === correctSequence[2].id
-  //   ) {
-  //     // console.log('correct');
-  //     setShowSuccessPopup(true);
-  //   } else {
-  //     // console.log("incorrect");
-  //     setShowWrongPopup(true); // Show wrong popup
-  //   }
-
-  //   // if(result.length>=3){
-  //   //   console.log(result);
-
-  //   // }
-  // };
-
-  // const handleBoxClick = () => {
-  //   if (selectedCards1 && selectedCards2 && selectedCards3) {
-  //     const userSequence = [selectedCards1, selectedCards2, selectedCards3];
-  //     const correctSequenceIds = correctSequence.map((card) => card.id);
-  //     const userSequenceIds = userSequence.map((card) => card.id);
-  //     if (userSequenceIds.join(",") === correctSequenceIds.join(",")) {
-  //       setShowSuccessPopup(true); // Show success popup
-  //     } else {
-  //       setShowWrongPopup(true); // Show wrong popup
-  //     }
-  //   }
-  // };
-
-  // Function to select a card from the deck
   const selectCard = (card, boxSetter) => {
     if (!card || !card.text) return;
-    // Set the selected card in the respective empty box
     if (!selectedCards1.text) {
       setSelectedCards1(card);
     } else if (!selectedCards2.text) {
@@ -237,39 +107,24 @@ const Level11 = ({ setCompletedLevels }) => {
       return;
     }
 
-    // Remove selected card from deck and show the next card
     const newDeck = deck.filter((c) => c.id !== card.id);
     setDeck(newDeck);
     if (newDeck.length > 0) {
-      setDeckIndex(0); // Show the first card from the remaining deck
+      setDeckIndex(0);
     } else {
-      setDeckIndex(null); // No more cards left in the deck
-    }
-  };
-
-  // Function to move to the next card in the deck
-  const showNextCard = () => {
-    if (deckIndex === null) {
-      setDeckIndex(0); // Show the first card on the first click
-    } else if (deckIndex < deck.length - 1) {
-      setDeckIndex(deckIndex + 1); // Show the next card
-    } else {
-      setDeckIndex(0); // Reset to the first card when the deck ends
+      setDeckIndex(null);
     }
   };
 
   const res = () => {
-    // Create an array of selected cards
     const selectedCards = [
       selectedCards1.text,
       selectedCards2.text,
       selectedCards3.text,
     ];
 
-    // Create an array of correct cards
     const correctCards = correctSequence.map((card) => card.text);
 
-    // Check if all selected cards exist in the correct sequence (regardless of order)
     const isCorrect = selectedCards.every((selectedCard) =>
       correctCards.includes(selectedCard)
     );
@@ -280,7 +135,7 @@ const Level11 = ({ setCompletedLevels }) => {
       localStorage.setItem("level11Result", JSON.stringify(selectedCards));
     } else {
       console.log("incorrect");
-      setShowWrongPopup(true); // Show wrong popup
+      setShowWrongPopup(true);
     }
   };
 
@@ -290,34 +145,12 @@ const Level11 = ({ setCompletedLevels }) => {
   };
 
   const resetGame = () => {
-    // setCountdown(1000);
-    // Reset the selected cards
     setSelectedCards1({});
     setSelectedCards2({});
     setSelectedCards3({});
-    setDeck(initialDeck); // Reset to the first card in the deck
+    setDeck(initialDeck);
     setDeckIndex(0);
-
-    // Reshuffle the deck
-    // const reshuffledDeck = shuffle(Array.from(initialDeck.entries()));
-    // setDeck(reshuffledDeck);
   };
-
-  // const handleWrongClose = () => {
-  //   // setShowWrongPopup(false);
-  //   // Optional: Reset the selected cards here if necessary
-  // };
-
-  // const codeSelection = () => {
-  //   const level3Result = JSON.parse(localStorage.getItem("level3Result")) || [];
-  //   for (let i = 0; i < level3Result.length; i++) {
-  //     if (level3Result[i] === "H") {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  //   // console.log(level3Result);
-  // };
 
   return (
     <div
@@ -327,20 +160,15 @@ const Level11 = ({ setCompletedLevels }) => {
         backgroundSize: "cover",
       }}
     >
-      {/* Star count on the top-left corner */}
-      <div className="absolute top-4 left-4 flex items-center gap-4">
+      <div className="absolute top-10 left-4 flex items-center gap-4">
         <div className="flex items-center gap-2">
           <FaStar className="text-yellow-500 text-xl sm:text-2xl" />
           <span className="text-slate-50 text-sm sm:text-base">{starCount}</span>
         </div>
       </div>
-      {/* Icons on the top-right corner */}
-      <div className="absolute top-4 right-4 flex items-center gap-4">
+      <div className="absolute top-10 right-4 flex items-center gap-4">
         <div className="flex items-center gap-2 cursor-pointer">
           <FaClock className="text-slate-50 text-xl sm:text-2xl" />
-          {/*<h2 className="text-xl text-blue-600 font-bold">
-           {countdown} s
-          </h2>*/}
         </div>
         <div className="flex items-center gap-2 cursor-pointer">
           <FaQuestionCircle className="text-slate-50 text-xl sm:text-2xl" />
@@ -348,26 +176,23 @@ const Level11 = ({ setCompletedLevels }) => {
         </div>
       </div>
       <div className="flex items-center justify-between w-full">
-        {/* <h2 className="text-xl font-bold mx-auto mr-54">Choose card from deck</h2> */}
-        <h2 className="text-2xl font-bold text-slate-50 mx-auto mr-50 mb-6">
+        <h2 className="text-2xl font-bold text-slate-50 mx-auto mt-10">
           Options available when initial WBCT comes clotted
         </h2>
       </div>
 
-      {/* Deck Display */}
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-x-4 gap-y-4 mb-20 items-center mx-auto">
   {deck.map((card) => (
     <div
       key={card.id}
-      className="border w-48 h-32 border-blue-500 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200 flex justify-center items-center"
-      onClick={() => selectCard(card)} // Use selectCard to handle selection and removal
+      className="border w-48 h-32 border-blue-500 p-4 bg-gray-100 rounded-lg text-center cursor-pointer hover:bg-gray-200"
+      onClick={() => selectCard(card)}
     >
-      <p className="text-center">{card.text}</p>
+      <p className="text-xs break-words text-center">{card.text}</p>
     </div>
   ))}
 </div>
 
-      {/* Selected Boxes */}
       <div className="text-xl w-full h-30">
         <div>
           <h2 className="text-slate-50 text-center text-2xl font-bold">
@@ -387,63 +212,54 @@ const Level11 = ({ setCompletedLevels }) => {
         </div>
       </div>
 
-        {/* <div className="flex w-full mt-10">
-          <h2 className="text-xl text-blue-600 font-bold">
-            Time Remaining: {countdown} seconds
-          </h2>
-        </div> */}
+      {showSuccessPopup && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md text-center">
+            <h2 className="text-2xl font-bold text-green-600 mb-4">
+              Your options are correct
+            </h2>
+            <button
+              onClick={() => {
+                handleCompleteLevel11();
+                navigate("/level12");
+              }}
+              className="mt-4 bg-amber-950 text-white px-4 py-2 rounded-lg "
+            >
+              Situation: Not clotted in any occasion
+            </button>
 
-        {/* Success Popup for Correct Sequence */}
-        {showSuccessPopup && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md text-center">
-              <h2 className="text-2xl font-bold text-green-600 mb-4">
-                Your options are correct
-              </h2>
-              <button
-                onClick={() => {
-                  handleCompleteLevel11();
-                  navigate("/level12");
-                }} // Redirect to Level 12
-                className="mt-4 bg-amber-950 text-white px-4 py-2 rounded-lg "
-              >
-                Situation: Not clotted in any occasion
-              </button>
-
-              <button
-                onClick={() => {
-                  handleCompleteLevel11();
-                  navigate("/level15");
-                }} // Redirect to Level 15
-                className="mt-4 bg-amber-950 text-white px-4 py-2 rounded-lg "
-              >
-                Situation: Clotted in all occasions
-              </button>
-            </div>
+            <button
+              onClick={() => {
+                handleCompleteLevel11();
+                navigate("/level15");
+              }}
+              className="mt-4 bg-amber-950 text-white px-4 py-2 rounded-lg "
+            >
+              Situation: Clotted in all occasions
+            </button>
           </div>
-        )}
+        </div>
+      )}
 
-        {/* Wrong Popup for Incorrect Sequence */}
-        {showWrongPopup && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md text-center">
-              <h2 className="text-2xl font-bold text-red-400 mb-4">
-                Your choices are incorrect
-              </h2>
-              {/* <p className="mb-6">You have selected the wrong sequence.</p> */}
-              <button
-                className="bg-red-400 text-white px-4 py-2 rounded-md"
-                onClick={() => {
-                  setShowWrongPopup(false);
-                  resetGame();
-                }}
-              >
-                Try Again
-              </button>
-            </div>
+      {showWrongPopup && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md text-center">
+            <h2 className="text-2xl font-bold text-red-400 mb-4">
+              Your choices are incorrect
+            </h2>
+            <button
+              className="bg-red-400 text-white px-4 py-2 rounded-md"
+              onClick={() => {
+                setShowWrongPopup(false);
+                resetGame();
+              }}
+            >
+              Try Again
+            </button>
           </div>
-        )}
-      </div>
+        </div>
+      )}
+    </div>
   );
 };
 
